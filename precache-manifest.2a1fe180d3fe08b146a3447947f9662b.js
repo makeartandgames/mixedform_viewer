5c58991476da5d3d1677ed8dd18ed236f2d6c0e3
makeartandgames <bigmwp@gmail.com>
2025-04-22 22:10:55 +0000

    Updates

---
+++ precache-manifest.2a1fe180d3fe08b146a3447947f9662b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0310ead097d2b04765f6cc068b457f98",
+    "url": "/mixedform_viewer/index.html"
+  },
+  {
+    "revision": "2575a13ce71dbac9b146",
+    "url": "/mixedform_viewer/static/css/main.a2bdb47a.chunk.css"
+  },
+  {
+    "revision": "55272cad3a51362d42fd",
+    "url": "/mixedform_viewer/static/js/2.753a1ece.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/mixedform_viewer/static/js/2.753a1ece.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2575a13ce71dbac9b146",
+    "url": "/mixedform_viewer/static/js/main.79d5b78c.chunk.js"
+  },
+  {
+    "revision": "0af86bb55abd9e73f06c",
+    "url": "/mixedform_viewer/static/js/runtime-main.766f9cfa.js"
+  }
+]);
